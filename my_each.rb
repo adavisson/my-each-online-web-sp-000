e86963db5d86228d8dddd33551af0d2332182eed
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 00:37:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,10 @@ def my_each(arr) # put argument(s) here
   if block_given?
     i = 0
     while i < arr.length
-      
+      yield arr[i]
       i += 1
     end
   else
     puts "Block not given."
-    
+  end
 end
