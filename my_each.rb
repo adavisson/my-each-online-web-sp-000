59c3c17f40c3ac5362df299034f74604b1890ace
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 00:37:15 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
+def my_each(arr) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < arr.length
+      
+      i += 1
+    end
+  else
+    puts "Block not given."
+    
 end
